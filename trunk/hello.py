c246ff3cbc8de65fffdc47efab73156ea38a0993
teancum37 <teancum37@d39c998a-0698-11df-8e78-e7f5014194b1>
2010-01-21 16:31:57 +0000

    sdaf

--- trunk/hello.py
+++ trunk/hello.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
+import cgitb
+cgitb.enable()
 
 # Required header that tells the browser how to render the text.
 print "Content-Type: text/plain\n\n"
 
-# Print a simple message to the display window.
-print "Hello, World!\n" 
+form = cgi.FieldStorage()
+if "name" not in form or "addr" not in form:
+    print "<H1>Error</H1>"
+    print "Please fill in the name and addr fields."
+    return
+print "<p>name:", form["name"].value
+print "<p>addr:", form["addr"].value
+

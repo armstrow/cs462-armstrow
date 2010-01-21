1e3780e7e61aca788ee52d8147214cc6911b44bd
teancum37 <teancum37@d39c998a-0698-11df-8e78-e7f5014194b1>
2010-01-21 16:09:09 +0000

    temp

--- server-config.py
+++ server-config.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
+
 print "Configure Server Started!!!"
 from shutil import copy, copytree, move
 import subprocess
  
 #the copy and copytree methods can help you move stuff out of your expanded package to various places on the server.
  
-#copytree("/tmp/bootstrap/servers/webserver", "/var/www/html/webserver")
+copytree("~/project-code", "/var/www/html/webserver")
  
 #executing commandline calls can be done with the subprocess module
 #subprocess.call(["yum -y install some-package-I-forgot-in-my-AMI",""], shell=True)

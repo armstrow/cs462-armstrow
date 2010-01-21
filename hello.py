57312d89a10fe1049b08ed3ecba0c82dfec65e6e
teancum37 <teancum37@d39c998a-0698-11df-8e78-e7f5014194b1>
2010-01-21 15:15:48 +0000

    a

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print "hello world"
+#!/usr/bin/python
+
+# Required header that tells the browser how to render the text.
+print "Content-Type: text/plain\n\n"
+
+# Print a simple message to the display window.
+print "Hello, World!\n" 
